8ce7a2e48e8aff4ef3b42e1114b5e4ee692c4de4
rjs4013 <rjs401323@gmail.com>
2024-09-03 23:47:03 +0900

    123132

--- 0903_greedy/5201_container_carrying.py
+++ 0903_greedy/5201_container_carrying.py
@@ -13,21 +13,35 @@ for test_case in range(1, T+1):
     weight = list(map(int, input().split()))
     truck = list(map(int, input().split()))
 
-    # 트럭의 적재용량과 컨테이너의 무게를 비교, 가장 작은 무게차이를 가진 컨테이너를 트럭에 실으면 최대값이 된다.
-    real_gotruck = [0]
+    # 그리디로 풀기기
+    weight.sort(reverse=True)
+    truck.sort(reverse=True)
+
+    total = 0
     for i in truck:
-        mpos = 100
-        gotruck = 0
         for j in weight:
-            if i < j: continue
-            pos = i - j
-            if mpos > pos:
-                mpos = pos
-                gotruck = j
-        if gotruck != 0:
-            real_gotruck.append(gotruck)
-            weight.pop(weight.index(gotruck))
-    print(f'#{test_case} {sum(real_gotruck)}')
+            if i >= j:
+                total += j
+                weight.remove(j)
+                break
+    print(f'#{test_case} {total}')
+
+
+    # # 트럭의 적재용량과 컨테이너의 무게를 비교, 가장 작은 무게차이를 가진 컨테이너를 트럭에 실으면 최대값이 된다.
+    # real_gotruck = [0]
+    # for i in truck:
+    #     mpos = 100
+    #     gotruck = 0
+    #     for j in weight:
+    #         if i < j: continue
+    #         pos = i - j
+    #         if mpos > pos:
+    #             mpos = pos
+    #             gotruck = j
+    #     if gotruck != 0:
+    #         real_gotruck.append(gotruck)
+    #         weight.pop(weight.index(gotruck))
+    # print(f'#{test_case} {sum(real_gotruck)}')
 
 
 
