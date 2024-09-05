590819f8d5604dd2198d32101815745218b7cd9d
rjs4013 <rjs401323@gmail.com>
2024-09-05 13:54:56 +0900

    133245

--- sorteer/visit_lv3.py
+++ sorteer/visit_lv3.py
@@ -1,7 +1,10 @@
-def dfs(si, sj):
+def dfs(si, sj, i, j):
     global ans
+    arr[stamp[i][0]][stamp[j][1]] = 0
     visited[si][sj] = 1
-    if si == stamp[M-1][0] and sj == stamp[M-1][1]:
+    if si == stamp[i][0]-1 and sj == stamp[j][1]-1:
+        if i+1 < N:
+            dfs(i, j, stamp[i+1][0]-1, stamp[j+1][1]-1)
         ans += 1
         return
 
@@ -10,8 +13,8 @@ def dfs(si, sj):
     for dx, dy in dxy:
         a = si + dx
         b = sj + dy
-        if 0<=a<N and 0<=b<N and visited[a][b] != 1 and arr[a][b] != 1:
-            dfs(a, b)
+        if 0<=a<N and 0<=b<N and visited[a][b] != 1 and arr[a][b] != 1 and arr[a][b] != -1:
+            dfs(a, b, i, j)
             visited[a][b] = 0
 
 
@@ -21,6 +24,10 @@ arr = [list(map(int, input().split())) for _ in range(N)]
 stamp = [list(map(int, input().split())) for _ in range(M)]
 
 visited = [[0] * N for _ in range(N)]
+
+for j in range(M):
+    arr[stamp[j][0]-1][stamp[j][1]-1] = -1
+
 ans = 0
-dfs(stamp[0][0] -1, stamp[0][1] -1)
+dfs(stamp[0][0] -1, stamp[0][1] -1, 1, 1)
 print(ans)
